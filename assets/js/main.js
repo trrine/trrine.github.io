ccd1cf59763c6f163a11de4a8061b0074760ed6d
Trine <41973043+trrine@users.noreply.github.com>
2023-08-20 00:02:33 +1000

    Add async javascript

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,3 +3,44 @@ function getEmailLink() {
     let domain = "gmail.com";
     return "mailto:" + local + "@" + domain;
 }
+
+function wait(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms))
+}
+
+async function typeWord(word, id, delay=100) {
+    const letters = word.split("");
+    let i = 0;
+
+    while (i < letters.length) {
+        await wait(delay);
+        document.getElementById(id).innerHTML += letters[i];
+        i++;        
+    }
+
+    return;
+}
+
+async function deleteWord(id, delay=100) {
+    const word = document.getElementById(id);
+    const letters = word.innerHTML.split("");
+    let i = 0;
+
+    while (letters.length > 0) {
+        await wait(delay);
+        letters.pop();
+        word.innerHTML = letters.join("");
+        i--;
+    }
+}
+
+async function wordAnimation(word, id, delay=100) {
+    while (true) {
+        await typeWord(word, id, delay);
+        await wait(1500);
+        await deleteWord(id, delay);
+        await wait(500);
+    }
+}
+
+wordAnimation("Trine.", "name", 200);
