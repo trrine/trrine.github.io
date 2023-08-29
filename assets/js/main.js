31e9fc4eb6cc008b3ee1ce068f80a3c61a2f101d
Trine <41973043+trrine@users.noreply.github.com>
2023-08-29 19:46:12 +1000

    Add project 2

--- assets/js/main.js
+++ assets/js/main.js
@@ -51,31 +51,41 @@ wordAnimation("Trine.", "name", 200);
 
 // SLIDESHOW
 
-let slideIndex = 1;
-showSlides(slideIndex);
+let slideIndex = [1, 1];
+let slideId = ["slides-1", "slides-2"];
+let dotId = ["slides-1__dot", "slides-2__dot"];
 
-// Next/previous controls
-function plusSlides(n) {
-  showSlides(slideIndex += n);
+function showSlides(n, no) {
+    let images = document.getElementsByClassName(slideId[no]);
+    let dots = document.getElementsByClassName(dotId[no]);
+  
+    if (n > images.length) {
+        slideIndex[no] = 1
+    }
+
+    if (n < 1) {
+        slideIndex[no] = images.length;
+    }
+    
+    for (let i = 0; i < images.length; i++) {
+        images[i].style.display = "none";
+    }
+
+    for (let i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    
+    images[slideIndex[no] - 1].style.display = "block";
+    dots[slideIndex[no] - 1].className += " active";
 }
 
-// Thumbnail image controls
-function currentSlide(n) {
-  showSlides(slideIndex = n);
+function plusSlides(n, no) {
+    showSlides(slideIndex[no] += n, no);
 }
 
-function showSlides(n) {
-  let i;
-  let slides = document.getElementsByClassName("slides");
-  let dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
+function currentSlide(n, no) {
+    showSlides((slideIndex[no] = n), no);
 }
+
+showSlides(1, 0);
+showSlides(1, 1);
